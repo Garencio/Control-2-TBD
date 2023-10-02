9ef650c2835f155e8bd2c9297642a257fcb28f92
Benjamin Pavez <benjamin.pavez.l@usach.cl>
2023-10-02 14:31:13 -0300

    Primeros archivos

---
+++ src/main/java/com/control_2_tbd/Control2TbdApplication.java
@@ -0,0 +1,13 @@
+package com.control_2_tbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control2TbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Control2TbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/control_2_tbd/Control2TbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control_2_tbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control2TbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

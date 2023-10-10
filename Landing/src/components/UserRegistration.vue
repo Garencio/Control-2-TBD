b16d1bce213e6c787aca7d4177032f24866c0abf
Jorge Morales <jorge.morales.r@usach.cl>
2023-10-10 19:17:41 -0300

    Mejora visual de la pagina
    Mejora aspectos para el registro y login

--- Landing/src/components/UserRegistration.vue
+++ Landing/src/components/UserRegistration.vue
@@ -68,7 +68,59 @@ export default {
 };
 </script>
 
-
 <style scoped>
-/* Aquí puedes agregar estilos específicos para el componente */
+/* Estilización para el componente de registro */
+
+div {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+h2 {
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+label {
+  margin-bottom: 0.3rem;
+  font-weight: 500;
+}
+
+input {
+  width: 100%;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  font-size: 1rem;
+  outline: none;
+}
+
+input:focus {
+  border-color: #007BFF;
+}
+
+button {
+  width: 100%;
+  padding: 0.5rem 0;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
 </style>
